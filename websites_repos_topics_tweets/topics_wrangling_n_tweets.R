5d1626681e3ec1f345d022feb4b18a7b45eb2f10
Paloma <palolili@gmail.com>
2020-03-05 00:29:41 +0100

    small updates to clean and export final data "data_with_topic"

--- websites_repos_topics_tweets/topics_wrangling_n_tweets.R
+++ websites_repos_topics_tweets/topics_wrangling_n_tweets.R
@@ -1,12 +1,13 @@
 library(tidyverse)
-
+library(rio)
+library(here)
 
 # Clean data --------------------------------------------------------------
 # data_files <- import("filesRLadiesrepos.csv")
 # data_files <- filesRLadiesrepos
 # data_files <- fulldata_final
 
-data_files <- import("github_repo_files.csv")
+data_files <- read_csv(here("websites_repos_topics_tweets", "github_repo_files.csv"))
 
 data_files <- data_files %>% 
   mutate(repo = str_extract(repo, "_(.*)"),
@@ -49,7 +50,7 @@ data_files <- data_files %>%
 ## Una idea podria ser qué repos tienen contenido sombre x tema?
 # perfecto, usé la nube de palabras para determinar los temas 
 
-data_test <- data_files %>%
+data_with_topic <- data_files %>%
   mutate(
     topic = ifelse(str_detect(name, "intro|Básico|beggin|Novice|scratch|Scratch"), "Intro", NA),
     topic = ifelse(str_detect(name, "ggplot|Ggplot"), "ggplot", topic),
@@ -73,10 +74,14 @@ data_test <- data_files %>%
 
 
 #Filtramos el repo de Global porque tiene charlas sobre R-Ladies.
-data_with_topic <- data_test %>%
-  filter(repo == 'Global_presentation') %>%
-  filter(!is.na(topic))
+data_with_topic <- data_with_topic %>%
+  filter(!is.na(topic)) %>% 
+  rename(City = repo) %>% 
+  mutate(City = ifelse(City == "La", "LA", City),
+         City = ifelse(City == "Rtp", "RTP", City),
+         City = ifelse(City == "Dc", "DC", City))
 
+readr::write_csv(data_with_topic, path = here::here("websites_repos_topics_tweets", "data_with_topic.csv"))
 
 #Podríamos filtrar por tema para armar las frases??? 
 
@@ -86,7 +91,7 @@ data_with_topic %>%
 
 data_with_topic %>% 
   filter(topic == "ggplot") %>% 
-  select(name)
+  select(html_url)
 
 data_with_topic %>% 
   filter(topic == "shiny") %>% 
@@ -95,3 +100,4 @@ data_with_topic %>%
 data_with_topic %>% 
   filter(topic == "tidyverse") %>% 
   select(name)
+
