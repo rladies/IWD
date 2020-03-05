87b5c114eae8232cabf1d05b451da499cfa0956a
Paloma <palolili@gmail.com>
2020-03-05 01:20:17 +0100

    upload 4 topics

--- websites_repos_topics_tweets/repos_website.R
+++ websites_repos_topics_tweets/repos_website.R
@@ -31,13 +31,16 @@ dataTw <- mutate(
     data,
     tweet = stringr::str_replace(sentences, "chapter name", data$City),
     tweet = paste0(tweet, extra_text, {{variable}}),
-    tweet = paste(tweet, "#rladies #iwd2020"),
-    tweet = paste(tweet, " @", str_sub(data$Twitter, 21, str_length(data$Twitter)),sep=""),
-    picture = paste("Chapter", str_replace_all(data$City, fixed(" "), ""), ".png",sep="")
-  )
+    tweet = paste(tweet, "#rladies #iwd2020"))
+
+attach(dataTw)
+if(exists("Twitter")){
+  dataTw <- dataTw %>% 
+    mutate(tweet = paste(tweet, " @", str_sub(data$Twitter, 21, str_length(data$Twitter)),sep=""))}
+detach(dataTw)
 
 # save tweets
-tweets <- dplyr::select(dataTw, City, tweet, picture) %>%
+tweets <- dplyr::select(dataTw, City, tweet) %>% 
   arrange(City)
 Encoding(tweets$City) <- "UTF-8"
 Encoding(tweets$tweet) <- "UTF-8"
@@ -102,3 +105,131 @@ website_tweets <- create_tweets(website_data,
                                     extra_text = " Website here: ")
 
 readr::write_csv(website_tweets, path = here::here("websites_repos_topics_tweets", "website_tweets.csv"))
+
+
+# Topics GGPLOT------------------------------------------------------------------
+data_with_topic <- read_csv(here::here("websites_repos_topics_tweets", "data_with_topic.csv"))
+
+ggplot_data <- data_with_topic %>% 
+  filter(topic == "ggplot") %>% 
+  filter(!is.na(City)) %>% 
+  filter(!str_detect(html_url, ".Rmd")) %>% ## Istambul had 2 rows with same content, 1 .R and one .Rmd
+  select(City, html_url)
+
+adjectives <- c("awesome", "fantastic", "wonderful", "amazing",  "phenomenal", 
+                "remarkable", "incredible",  "magnificent", "marvelous", 
+                "insightful", "admirable", "outstanding", "splendid", "exceptional", 
+                "intuitive", "brilliant", "excellent", "cool", "super", "epic")
+
+ggplot_templates <-
+  c(
+    "Learn about the grammar of graphics with #ggplot! Check chapter name adjective materials!",
+    "Feeling like learning about #datavis with #ggplot? chapter name chapter has some adjective materials on it!",
+    "Because plots with #ggplot2 are adjective plots! Visit the chapter name's materials on the topic!",
+    "Need to prepare a workshop on #ggplot? Browse the adjective chapter name's materials for inspiration!",
+    "#Datavis is fun with #ggplot! Check chapter name adjective materials for extra fun!",
+    "Looking for materials in #ggplot? Check chapter name's adjective materials!"
+    )
+
+
+ggplot_tweets <- create_tweets(ggplot_data,
+                                ggplot_templates,
+                                adjectives,
+                                variable = html_url,
+                                extra_text = " ")
+
+
+readr::write_csv(ggplot_tweets, path = here::here("websites_repos_topics_tweets", "ggplot_tweets.csv"))
+
+
+# Topics Shiny ------------------------------------------------------------
+
+# data_with_topic <- read_csv(here::here("websites_repos_topics_tweets", "data_with_topic.csv"))
+
+shiny_data <- data_with_topic %>% 
+  filter(topic == "shiny") %>% 
+  filter(!is.na(City)) %>% 
+  filter(!str_detect(html_url, "_images.zip")) %>% ## Istambul had 2 rows with same content
+  select(City, html_url)
+
+adjectives <- c("awesome", "fantastic", "wonderful", "amazing",  "phenomenal", 
+                "remarkable", "incredible",  "magnificent", "marvelous", 
+                "insightful", "admirable", "outstanding", "splendid", "exceptional", 
+                "intuitive", "brilliant", "excellent", "cool", "super", "epic")
+
+shiny_templates <-
+  c(
+    "Take your skills to the next level with #shiny! Check chapter name adjective materials!",
+    "Want to build your first #Shiny app? chapter name chapter has some adjective materials on it!",
+    "Get your hands on #Shiny dashboards! Visit the chapter name's adjective materials on the topic!",
+    "Need to prepare a workshop on #Shiny Apps? Browse the adjective chapter name's materials for inspiration!",
+    "Learn how to create a #Shiny web app in R! Check chapter name adjective materials on it!",
+    "Check the basics of building #Shiny apps in R at chapter name's adjective materials!"
+  )
+
+
+shiny_tweets <- create_tweets(shiny_data,
+                               shiny_templates,
+                               adjectives,
+                               variable = html_url,
+                               extra_text = " ")
+
+
+readr::write_csv(shiny_tweets, path = here::here("websites_repos_topics_tweets", "shiny_tweets.csv"))
+
+# Topics Rmarkdown ------------------------------------------------------------
+
+# data_with_topic <- read_csv(here::here("websites_repos_topics_tweets", "data_with_topic.csv"))
+
+rmarkdown_data <- data_with_topic %>% 
+  filter(topic == "rmarkdown") %>% 
+  filter(!is.na(City)) %>% 
+  select(City, html_url)
+  
+rmarkdown_templates <-
+  c(
+    "Take your skills to the next level with #rmarkdown! Check chapter name adjective materials!",
+    "Want to make reproducible reports with #rmarkdown? chapter name chapter has some adjective materials on it!",
+    "Forget about copy-pasting outputs, learn #rmarkdown and make your research reproducible! Check chapter name adjective materials",
+    "Need to prepare a workshop on #Rmarkdown? Browse the adjective chapter name's materials for inspiration!",
+    "Help your future self by making reproducible reports with #Rmardown and pick chapter name's adjective materials!"
+  )
+
+
+rmarkdown_tweets <- create_tweets(rmarkdown_data,
+                                  rmarkdown_templates,
+                              adjectives,
+                              variable = html_url,
+                              extra_text = " ")
+
+
+readr::write_csv(rmarkdown_tweets, path = here::here("websites_repos_topics_tweets", "rmarkdown_tweets.csv"))
+
+# Topics Tidyverse ------------------------------------------------------------
+
+# data_with_topic <- read_csv(here::here("websites_repos_topics_tweets", "data_with_topic.csv"))
+
+tidyverse_data <- data_with_topic %>% 
+  filter(topic == "tidyverse") %>% 
+  filter(!is.na(City)) %>% 
+  select(City, html_url)
+
+tidyverse_templates <-
+  c(
+    "Are you ready to master the %>% pipe with name chapter adjective materials on #tidyverse!",
+    "Master your data science skills with the #tidyverse ! Check chapter name adjective materials",
+    "Are you taking your first R steps? The #tidyverse will make this journey more fun! Check chapter name's adjective materials!",
+    "Learn all you need to know about the #tidyverse! Check chapter name adjective materials!",
+    "Need to prepare a workshop on #Tidyverse? Browse the adjective chapter name's materials for inspiration!"
+  )
+
+
+tidyverse_tweets <- create_tweets(tidyverse_data,
+                                  tidyverse_templates,
+                                  adjectives,
+                                  variable = html_url,
+                                  extra_text = " ")
+
+
+readr::write_csv(tidyverse_tweets, path = here::here("websites_repos_topics_tweets", "tidyverse_tweets.csv"))
+
