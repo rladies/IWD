fed5bc9f9d00998fe4a7b7693afb99c794d8b9c4
nrennie <nrennie35@gmail.com>
2024-03-06 17:04:48 +0000

    add rest of plots

--- plots/chapters_maps/2024-iwd-maps.R
+++ plots/chapters_maps/2024-iwd-maps.R
@@ -1,7 +1,7 @@
 library(tidyverse)
 library(jsonlite)
 chapters_data <- jsonlite::fromJSON("https://raw.githubusercontent.com/rladies/meetup_archive/main/data/chapters_meetup.json")
-events_data <- jsonlite::fromJSON('https://raw.githubusercontent.com/rladies/meetup_archive/main/data/events.json')
+events_data <- jsonlite::fromJSON("https://raw.githubusercontent.com/rladies/meetup_archive/main/data/events.json")
 
 # Map of chapters ---------------------------------------------------------
 
@@ -51,14 +51,62 @@ ggplot() +
 
 ggsave("plots/chapters_maps/2024-chapter-map.png", width = 5.5, height = 3, bg = "white")
 
-select(1,2,3,7,8)%>% 
-  rename(chapter=group_urlname)%>%
-  mutate(location=ifelse(location=="Online event","online","inperson"),
-         title=sub(".*-- ","",title),
-         title=gsub("\\s*\\([^\\)]+\\)","",title)) %>%
-  filter(!str_detect(title,regex("canceled|cancelled",ignore_case=T)),
-         !chapter%in%c("RLadiesJeddah","muhq_deleted@4633@rladies-ushuaia",
-                       "muhq_deleted@9919@notopic@508502","notopic@544550"))%>%
-  arrange(desc(date))%>%
-  filter(year(date)<2024)%>%
-  mutate(year=year(date))
+# Events bar chart --------------------------------------------------------
+
+events_bar_data <- events_data |>
+  mutate(year = as.character(year(date))) |>
+  count(year) |>
+  filter(year != 2038)
+
+ggplot(events_bar_data) +
+  geom_col(aes(x = year, y = n), fill = "#88398A") +
+  geom_text(aes(x = year, y = n + 40, label = n), colour = "#88398A") +
+  labs(title = "R-Ladies Events", x = "", y = "Number of events",
+       caption = "Source: meetup.com (March 2024)") +
+  theme_minimal(base_size = 10) +
+  theme(
+    plot.margin = margin(10, 10, 10, 10),
+    plot.title = element_text(
+      face = "bold", size = rel(1.4),
+      colour = "#88398A",
+      margin = margin(
+        t = 0, r = 0, b = 6.6,
+        l = 0, unit = "pt"
+      )
+    ),
+    plot.caption = element_text(hjust = 0)
+  )
+
+ggsave("plots/chapters_maps/2024-events-bar.png", width = 5.5, height = 3, bg = "white")
+
+# Events bar chart --------------------------------------------------------
+
+first_events_bar_data <- events_data |>
+  group_by(group_urlname) |> 
+  slice_min(date) |> 
+  ungroup() |> 
+  mutate(year = as.character(year(date))) |>
+  count(year) |>
+  filter(year != 2038)
+
+ggplot(first_events_bar_data) +
+  geom_col(aes(x = year, y = n), fill = "#88398A") +
+  geom_text(aes(x = year, y = n + 8, label = n), colour = "#88398A") +
+  labs(title = "New R-Ladies Chapters", x = "", 
+       y = "Number of new chapters",
+       caption = "Source: meetup.com (March 2024)") +
+  theme_minimal(base_size = 10) +
+  theme(
+    plot.margin = margin(10, 10, 10, 10),
+    plot.title = element_text(
+      face = "bold", size = rel(1.4),
+      colour = "#88398A",
+      margin = margin(
+        t = 0, r = 0, b = 6.6,
+        l = 0, unit = "pt"
+      )
+    ),
+    plot.caption = element_text(hjust = 0)
+  )
+
+ggsave("plots/chapters_maps/2024-first-events-bar.png", width = 5.5, height = 3, bg = "white")
